72e998c2f8f4e4f5ce45008524860d1b02aa94aa
HongTao <hongtao@iie.ac.cn>
2019-10-18 12:44:27 +0800

    init project

---
+++ src/main/java/cn/ac/iie/sycnimage/SycnimageApplication.java
@@ -0,0 +1,13 @@
+package cn.ac.iie.sycnimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SycnimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SycnimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/ac/iie/sycnimage/SycnimageApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ac.iie.sycnimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SycnimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
